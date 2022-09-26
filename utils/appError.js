78703d9d5f75cce69d27255feee2c70888b34f65
Daphne <daphneruth304@gmail.com>
2022-09-26 20:10:03 +0300

    updated

--- utils/appError.js
+++ utils/appError.js
@@ -1,7 +1,6 @@
 class appError extends Error {
   constructor(message, statusCode) {
     super(message);
-
     this.statusCode = statusCode;
     this.message = `${statusCode}.startWith`("4") ? "fail" : "error";
   }

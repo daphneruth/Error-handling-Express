e4d331b6fc3105aabf6ac14bf8f9a772facbf132
Daphne <daphneruth304@gmail.com>
2022-10-03 07:03:52 +0300

    updated

--- models/tourModel.js
+++ models/tourModel.js
@@ -93,16 +93,6 @@ tourSchema.pre("save", function (next) {
   next();
 });
 
-// tourSchema.pre('save', function(next) {
-//   console.log('Will save document...');
-//   next();
-// });
-
-// tourSchema.post('save', function(doc, next) {
-//   console.log(doc);
-//   next();
-// });
-
 // QUERY MIDDLEWARE
 // tourSchema.pre('find', function(next) {
 tourSchema.pre(/^find/, function (next) {
